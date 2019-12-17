a03d362fd1c65ca11bfa2a007dda2c855f85f690
Daniil Zulin <d.zulin@g.nsu.ru>
2019-12-17 23:06:21 +0700

    New endpoint for attendance access

--- student-control/src/main/java/nsu/ccfit/studentcontrol/controllers/AttendanceRestController.java
+++ student-control/src/main/java/nsu/ccfit/studentcontrol/controllers/AttendanceRestController.java
@@ -1,20 +1,34 @@
 package nsu.ccfit.studentcontrol.controllers;
 
+import nsu.ccfit.studentcontrol.dto.Attendance;
+import nsu.ccfit.studentcontrol.dto.Student;
 import nsu.ccfit.studentcontrol.repos.AttendanceRepository;
+import nsu.ccfit.studentcontrol.repos.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/attendance", produces = "application/json")
 @CrossOrigin("*")
 public class AttendanceRestController {
+    private final StudentRepository studentRepository;
     private final AttendanceRepository attendanceRepository;
 
     @Autowired
-    public AttendanceRestController(AttendanceRepository attendanceRepository) {
+    public AttendanceRestController(StudentRepository studentRepository, AttendanceRepository attendanceRepository) {
+        this.studentRepository = studentRepository;
         this.attendanceRepository = attendanceRepository;
+    }
+
+    @GetMapping(path = "/student/{id}/")
+    public ResponseEntity<List<Attendance>> studentAttendance(@PathVariable(name = "id") int id) {
+        Student student = studentRepository.findById(id).orElse(null);
+        if (student == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(student.getAttendanceList(), HttpStatus.OK);
     }
 }
